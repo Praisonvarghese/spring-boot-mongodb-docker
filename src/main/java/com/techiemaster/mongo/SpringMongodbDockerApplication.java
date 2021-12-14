488aba95af1680e753fdd5713656facffb607deb
praison <praisonvarghese1@gmail.com>
2021-12-14 15:25:01 +0530

    Initial Commit

---
+++ src/main/java/com/techiemaster/mongo/SpringMongodbDockerApplication.java
@@ -0,0 +1,13 @@
+package com.techiemaster.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techiemaster/mongo/SpringMongodbDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techiemaster.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
